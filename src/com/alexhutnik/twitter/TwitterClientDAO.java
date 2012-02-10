5cd969e24e5fedce0bdb8b046055c8ba24baab60
Alex Hutnik <alex.hutnik@gmail.com>
2012-02-10 01:32:16 -0500

    - remove unnecessary comments

--- src/com/alexhutnik/twitter/TwitterClientDAO.java
+++ src/com/alexhutnik/twitter/TwitterClientDAO.java
@@ -49,32 +49,6 @@ public class TwitterClientDAO {
 	}
 
 	public void addTweet(String tweetId, String name, String text, String searchId) {
-		/*
-		 * Kind of a tricky situation here. I want to make sure I don't store a
-		 * single tweet more than once (i.e. Twitter returned the same tweet for
-		 * two different searches). But I also want to reduce the number of
-		 * times we hit the DB. Could be up to 3 queries for each tweet. Could
-		 * add up to quite a few.
-		 * 
-		 * Option 1: 
-		 * 1. Check DB for existing tweet (+1)
-		 * 2a. If it exists grab the row id
-		 * 2b. If not, insert the new tweet (+1)
-		 * 3. Insert entry in join table (+1) Up to (1+1+1) queries.
-		 * 
-		 * Option 2:
-		 * 1. Using a raw query, insert the new tweet with ON DUPLICATE KEY 1=1 (+1) 
-		 * 2. Since that returns an "empty" cursor, re-query to get the id (+1) 
-		 * 3. Insert row into join table (+1)
-		 * Guaranteed 3 queries.
-		 * 
-		 * So let's go with the first option. We'll likely hit 3 queries and in
-		 * some really far out edge case, we only need two.
-		 */
-		
-		/*
-		 * TODO Test to see if we have an entry in the join table too.
-		 */
 		ContentValues cv = new ContentValues();
 		database.beginTransaction();
 		Cursor cursor = database.query(SQLConstants.TABLES.TWEETS, new String[] { SQLConstants.COLUMNS.ID }, SQLConstants.COLUMNS.TWEET_ID + "=?", new String[]{tweetId}, null, null, null);
